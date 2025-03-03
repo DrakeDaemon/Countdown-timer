814397f11f82e2502feca4fd8cd826549a1b1809
DrakeDaemon <drakedaemon1@gmail.com>
2025-03-03 18:13:18 +0300

    Added selection of different ending sounds

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import time
 import winsound
 import customtkinter
 
-
+sound1 = 'waterdrop'
 
 class App(customtkinter.CTk):
     #* Gui
@@ -12,36 +12,42 @@ class App(customtkinter.CTk):
         self.title('Countdown timer')
         self.eval('tk::PlaceWindow . center')
         self.resizable(width=False, height=False)
-        self.geometry('160x140')
+        self.geometry('160x210')
         self.grid_rowconfigure((0, 1), weight=5)
         self.grid_columnconfigure((0, 1, 2), weight=5)
 
-        self.label = customtkinter.CTkLabel(self, text='Countdown:\n', font=('Arial', 15), width=30)
+        self.label = customtkinter.CTkLabel(self, text='Countdown:\n', font=('Arial', 15), width=30, justify='center')
         self.label.grid(row=0, column=0, padx=5, pady=5, sticky='new') 
         self.timer = customtkinter.CTkLabel(self, text='00:00', font=('Arial', 16), width=40)
-        self.timer.grid(row=0, column=0, padx=5, pady=4, sticky='sew')
+        self.timer.grid(row=0, column=0, padx=5, pady=25, sticky='n')
 
-        self.entry = customtkinter.CTkEntry(self, placeholder_text='Enter amount of seconds.', width=158)
-        self.entry.grid(row=1, column=0, padx=5, pady=0, sticky='w')
+        self.entry = customtkinter.CTkEntry(self, placeholder_text='Amount of seconds.', width=158)
+        self.entry.grid(row=4, column=0, padx=5, pady=0, sticky='w')
+
+        self.sound_label = customtkinter.CTkLabel(self, text='Select your sound:', font=('Arial', 12))
+        self.sound_label.grid(padx=5, row=2, column=0, sticky='w')
+        self.combo = customtkinter.CTkComboBox(self, values=['Water Drop', 'Loud', 'Notification', 'Success'])
+        self.combo.grid(padx=5, pady=5, column=0, row=3, sticky='sew')
 
         self.button = customtkinter.CTkButton(self, text='Start countdown', command=self.event, width=158)
-        self.button.grid(row=2, column=0, padx=5, pady=5, sticky='w')
+        self.button.grid(row=5, column=0, padx=5, pady=5, sticky='w')
 
     #* Timer
-    def countdown(self, usr_ti):
-        if usr_ti >= 0:
+    def countdown(self, usr_ti, sound1):
+        if usr_ti > 0:
             mins, sec = divmod(usr_ti, 60)
             timer = '{:02d}:{:02d}'.format(mins, sec)
             self.timer.configure(text=timer)
-            if usr_ti >= 0:
-                self.after(1000, self.countdown, usr_ti - 1)
+            self.after(1000, self.countdown, usr_ti - 1, sound1)
         else:
-            winsound.PlaySound(sound="SystemHand", flags=winsound.SND_ALIAS)
+            self.timer.configure(text='00:00')
+            winsound.PlaySound(sound=f"sounds\{sound1}.wav", flags=winsound.SND_ALIAS)
 
     def event(self):
         try:
+            sound1 = self.combo.get().lower().replace(' ', "")
             usr_ti = int(self.entry.get())
-            self.countdown(usr_ti)
+            self.countdown(usr_ti, sound1)
         except ValueError:
             self.timer.configure(text="Invalid input")
 
